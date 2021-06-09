a9cf0dcf6c0248239441c8431ab3f85585c20218
Anwesh43 <anweshthecool0@gmail.com>
2021-06-10 02:29:31 +0530

    adding constants and libraries

--- eitherlinecircleshooterview/src/main/java/com/example/eitherlinecircleshooterview/EitherLineCircleShooterView.kt
+++ eitherlinecircleshooterview/src/main/java/com/example/eitherlinecircleshooterview/EitherLineCircleShooterView.kt
@@ -1,2 +1,28 @@
 package com.example.eitherlinecircleshooterview
 
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#9C27B0",
+    "#01579B",
+    "#FFD600",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val rots : Int = 4
+val parts : Int = rots + 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 11.2f
+val rot : Float = 90f
