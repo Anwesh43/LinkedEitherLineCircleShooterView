26472d2bb8b347598750a1ea39c2669782cad7f4
Anwesh43 <anweshthecool0@gmail.com>
2021-06-10 03:31:55 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedeitherlinecircleshooterview/MainActivity.kt
+++ app/src/main/java/com/example/linkedeitherlinecircleshooterview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedeitherlinecircleshooterview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.eitherlinecircleshooterview.EitherLineCircleShooterView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        EitherLineCircleShooterView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
