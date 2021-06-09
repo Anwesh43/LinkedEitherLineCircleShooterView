bc6e826e72bef8d67f41c383ab1c5f75a2193cdb
Anwesh43 <anweshthecool0@gmail.com>
2021-06-10 03:04:36 +0530

    adding ELCSNode class

--- eitherlinecircleshooterview/src/main/java/com/example/eitherlinecircleshooterview/EitherLineCircleShooterView.kt
+++ eitherlinecircleshooterview/src/main/java/com/example/eitherlinecircleshooterview/EitherLineCircleShooterView.kt
@@ -129,4 +129,45 @@ class EitherLineCircleShooterView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ELCSNode(var i : Int, val state : State = State()) {
+
+        private var next : ELCSNode? = null
+        private var prev : ELCSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ELCSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawELCSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ELCSNode {
+            var curr : ELCSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
