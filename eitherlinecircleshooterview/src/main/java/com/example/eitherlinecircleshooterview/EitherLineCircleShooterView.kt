576a6e570f98fe0b95ac4f34817321aa1f2ff176
Anwesh43 <anweshthecool0@gmail.com>
2021-06-10 02:44:16 +0530

    adding Canvas extension methods

--- eitherlinecircleshooterview/src/main/java/com/example/eitherlinecircleshooterview/EitherLineCircleShooterView.kt
+++ eitherlinecircleshooterview/src/main/java/com/example/eitherlinecircleshooterview/EitherLineCircleShooterView.kt
@@ -30,3 +30,39 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawEitherLineCircleShooter(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val sc6 : Float = scale.divideScale(5, parts)
+    val sc7 : Float = scale.divideScale(6, parts)
+    val r : Float = Math.min(w, h) / rFactor
+    val upSize : Float = size * (sc1 - sc7)
+    var k : Float = 0f
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..3) {
+        val scj : Float = scale.divideScale(2 + j, parts)
+        k += rot * scj.divideScale(0, parts)
+        val x : Float = (h / 2 - size / 2 + r) * scj.divideScale(1, parts)
+        save()
+        rotate(k)
+        drawCircle(x, 0f, r * sc2, paint)
+        restore()
+    }
+    drawLine(-upSize / 2, 0f, upSize / 2, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawELCSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawEitherLineCircleShooter(scale, w, h, paint)
+}
